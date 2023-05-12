824ae6e55008a09d3535deeea81a66fdc2f874ba
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-05-13 00:45:48 +0300

    change ui of service buttons

--- src/website/src/landing/services.php
+++ src/website/src/landing/services.php
@@ -5,8 +5,8 @@
         <div class="box">
             <img src="<?php echo $assets ?>systree.jpg" alt="">
             <div class="info">
-                <a href="<?php echo $sys_tree ?>login.php"><?php echo language('LOGIN') ?></a>
-                <a href="<?php echo $website_pages_desc ?>sys_tree/index.php"><?php echo language('DETAILS') ?></a>
+                <a class="btn btn-primary" href="<?php echo $sys_tree ?>login.php"><?php echo language('LOGIN') ?></a>
+                <a class="btn btn-outline-success" href="<?php echo $website_pages_desc ?>sys_tree/index.php"><?php echo language('DETAILS') ?></a>
             </div>
         </div>
         
