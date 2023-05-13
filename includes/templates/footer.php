9a03b7d2cf45eef95fe4e550780062a6507dd688
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-05-14 01:11:22 +0300

    update footer file

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,32 +1,32 @@
 <?php
 if (isset($page_category)) {
-    
-    // switch case to include the specific footer of given page category
-    switch($page_category) {
-        case 'website':
-            // get footer
-            $website_footer = get_page_dependencies("".$page_category."_global", 'footer');
-            // include footer
-            include_once $website_tpl . $website_footer;
-            break;
-            
-        case 'sys_tree':
-            // check session of user
-            if (isset($_SESSION['UserID'])) {
-                if ($_SESSION['isRoot']) {
-                    $sys_tree_footer = get_page_dependencies("".$page_category."_global", 'footer')['root'];
-                } else {
-                    $sys_tree_footer = get_page_dependencies("".$page_category."_global", 'footer')['user'];
-                }
-                // include footer
-                include_once $sys_tree_tpl . $sys_tree_footer;
-            }
-            break;
-            
-        case 'blog':
-            $blog_footer = get_page_dependencies("".$page_category."_global", 'footer');
-            // include footer
-            include_once $blog_tpl . $blog_footer;
-            break;
+  
+  // switch case to include the specific footer of given page category
+  switch($page_category) {
+    case 'website':
+      // get footer
+      $website_footer = get_page_dependencies("".$page_category."_global", 'footer');
+      // include footer
+      include_once $website_tpl . $website_footer;
+      break;
+      
+    case 'sys_tree':
+      // check session of user
+      if (isset($_SESSION['UserID'])) {
+        if ($_SESSION['isRoot']) {
+          $sys_tree_footer = get_page_dependencies("".$page_category."_global", 'footer')['root'];
+        } else {
+          $sys_tree_footer = get_page_dependencies("".$page_category."_global", 'footer')['user'];
         }
+        // include footer
+        include_once $sys_tree_tpl . $sys_tree_footer;
+      }
+      break;
+      
+    case 'blog':
+      $blog_footer = get_page_dependencies("".$page_category."_global", 'footer');
+      // include footer
+      include_once $blog_tpl . $blog_footer;
+      break;
+    }
 }
