f2ef41a4a2a494f440c9067ccb828f7432ff4840
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-06-07 23:17:06 +0300

    select all employees that job title id = 2 to assign combinations to them

--- src/sys_tree/user/combinations/add-combination.php
+++ src/sys_tree/user/combinations/add-combination.php
@@ -49,7 +49,7 @@ $emp_counter = $db_obj->count_records("`UserID`", "`users`", "WHERE `isTech` = 1
                 <option  value="default" disabled selected><?php echo language('SELECT', @$_SESSION['systemLang'])." ".language('TECHNICAL NAME', @$_SESSION['systemLang']) ?></option>
                 <?php 
                 // get Employees ID and Names
-                $usersRows = $db_obj->select_specific_column("`UserID`, `UserName`", "`users`", "WHERE `isTech` = 1 AND `company_id` = ".$_SESSION['company_id']);
+                $usersRows = $db_obj->select_specific_column("`UserID`, `UserName`", "`users`", "WHERE `isTech` = 1 AND `job_title_id` = 2 AND `company_id` = ".$_SESSION['company_id']);
                 // check the length of result
                 if (count($usersRows) > 0) {
                   // loop on result ..
