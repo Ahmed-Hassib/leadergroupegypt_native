15cabf9166580d984081e1432d2ee61d40403708
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-06-09 02:16:30 +0300

    manage index file of complaints and suggestions that control all routes and files of this department

--- src/sys_tree/user/comp-sugg/index.php
+++ src/sys_tree/user/comp-sugg/index.php
@@ -9,79 +9,84 @@ ob_start();
 session_start();
 // regenerate session id
 session_regenerate_id();
+
+// level
+$level = 4;
+// nav level
+$nav_level = 1;
+// some page flages
+$possible_back = true;
+$preloader = true;
+$is_contain_table = false;
+
+// initial configration of system
+include_once str_repeat("../", $level) . "etc/pre-conf.php";
+// check username in SESSION variable
+if (isset($_SESSION['UserName']) && $_SESSION['isLicenseExpired'] == 0) {
+  // check if Get request do is set or not
+  $query = isset($_GET['do']) ? $_GET['do'] : 'manage';
+  // get type of the query
+  $type = isset($_GET['type']) ? intval($_GET['type']) : -1;
+  // condition
+  $condition = $type != -1 ? " WHERE `type` = " . $type : "";
+  // start manage page
+  if ($query == "manage") {       // manage page
+    // include comp & sugg dashboard page
+    $file_name = 'dashboard.php';
+    
+  } elseif ($query == 'personal-comp-sugg') {
+    // include personal comp & sugg page
+    $file_name = 'personal-comp-sugg.php';
+    $is_contain_table = true;
+    
+  } elseif ($query == 'show-comp-sugg') { 
+    // include show comp & sugg page
+    $file_name = 'show-comp-sugg.php';
+    $is_contain_table = true;
+    
+  } elseif ($query == 'delete-comp-sugg') {
+    // include delete comp & sugg page
+    $file_name = 'delete-comp-sugg.php';
+    $possible_back = false;
+    $preloader = false;
+    
+  } elseif ($query == 'add-comp-sugg') {
+    // include add comp & sugg page
+    $file_name = 'add-comp-sugg.php';
+    
+  } elseif ($query == "insert-comp-sugg") {
+    // include insert comp & sugg page
+    $file_name = 'insert-comp-sugg.php';
+    $possible_back = false;
+    $preloader = false;
+    
+  } else {
+    // include page error module
+    $file_name = $globmod . 'page-permission-error.php';
+    $possible_back = false;
+    $preloader = false;
+  }
+  
+} else {
+  // include permission error module
+  $file_name = $globmod . 'permission-error.php';
+  $possible_back = false;
+  $preloader = false;
+}
+
 // page title
 $page_title = "complaints & suggestions";
 // page category
 $page_category = "sys_tree";
 // page role
-$page_role = "sys_tree_user";
+$page_role = "sys_tree_comp";
 // folder name of dependendies
 $dependencies_folder = "sys_tree/";
-// level
-$level = 4;
-// nav level
-$nav_level = 1;
-// refere to that this page have tables
-$is_contain_table = true;
+
 // initial configration of system
 include_once str_repeat("../", $level) . "etc/init.php";
-// check username in SESSION variable
-if (isset($_SESSION['UserName']) && $_SESSION['isLicenseExpired'] == 0) {
-    // check if Get request do is set or not
-    $query = isset($_GET['do']) ? $_GET['do'] : 'manage';
-    // get type of the query
-    $type = isset($_GET['type']) ? intval($_GET['type']) : -1;
-    // condition
-    $condition = $type != -1 ? " WHERE `type` = " . $type : "";
-    // start manage page
-    if ($query == "manage") {       // manage page
-    
-        // include comp & sugg dashboard page
-        include_once 'dashboard.php';
-        
-    } elseif ($query == 'personalCompSugg') {
+include_once $file_name;
 
-        // include personal comp & sugg page
-        include_once 'personal-comp-sugg.php';
-        
-    } elseif ($query == 'showCompSugg' && $_SESSION['sugg_show'] == 1) { 
-        
-        // include show comp & sugg page
-        include_once 'show-comp-sugg.php';
-        
-    } elseif ($query == 'deleteCompSugg' && $_SESSION['sugg_delete'] == 1) {
-        
-        // include delete comp & sugg page
-        include_once 'delete-comp-sugg.php';
-        
-    } elseif ($query == 'activateSugg' && $_SESSION['sugg_replay'] == 1) {
-        
-        // include activate comp & sugg page
-        include_once 'activate-comp-sugg.php';
-        
-    } elseif ($query == 'addNewComSugg') {
-        
-        // include add comp & sugg page
-        include_once 'add-comp-sugg.php';
-        
-    } elseif ($query == "insertCompSugg") {
-        
-        // include insert comp & sugg page
-        include_once 'insert-comp-sugg.php';
-        
-    } else {
-        
-        // include page error module
-        include_once $globmod . 'page-error.php';
-        
-    }
-    
-} else {
-    
-    // include permission error module
-    include_once $globmod . 'permission-error.php';
-    
-}
 // include footer
 include_once $tpl . "footer.php"; 
 include_once $tpl . "js-includes.php";
