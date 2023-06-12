714d8b9cb87a4ac07306846fb1049c29d745fed7
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-06-12 18:38:33 +0300

    add links in footer for rating app and add a complaints or suggestions

--- includes/languages/arabic.php
+++ includes/languages/arabic.php
@@ -112,6 +112,7 @@ function languageAr($phrase)
     'THUS'                  => 'حيث',
     'GOOD LUCK'             => 'بالتوفيق',
     'POWERED BY'            => 'powered by',
+    'RATE APP'              => 'تقييم السيستم',
 
     
     
--- includes/templates/sys_tree/user-footer.php
+++ includes/templates/sys_tree/user-footer.php
@@ -1,8 +1,25 @@
-<footer>
-  <div class="mt-1 row text-center">
+<footer class="text-center">
+  <!-- application name -->
+  <div class="mt-1 row">
     <h3 class="fw-bold"><?php echo language('SYS TREE', @$_SESSION['systemLang']) ?></h3>
   </div>
-  <div class="row text-center fs-12">
+  <!-- rate app or create a complaint or suggestion -->
+  <div class="hstack gap-3" dir="<?php echo $_SESSION['systemLang'] == 'ar' ? 'rtl' : 'ltr' ?>">
+    <ul>
+      <li>
+        <a href="">
+          <?php echo language('RATE APP', @$_SESSION['systemLang']) ?>
+        </a>
+      </li>
+      <li>
+        <a href="">
+          <?php echo language('COMPLAINTS & SUGGESTIONS', @$_SESSION['systemLang']) ?>
+        </a>
+      </li>
+    </ul>
+  </div>
+  <!-- sponsor and developer name -->
+  <div class="row fs-12">
     <p class="text-uppercase">
       <span><?php echo language('POWERED BY', @$_SESSION['systemLang']) ?></span>
       <span><?php echo $developerName . " &amp; " . $sponsorCompany ?></span>
