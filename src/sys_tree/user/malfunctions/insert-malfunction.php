a7189b54929468061958883d07597dd05e9d58fc
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-06-28 01:47:21 +0300

    update insertion algorithm to use session flash alert

--- src/sys_tree/user/malfunctions/insert-malfunction.php
+++ src/sys_tree/user/malfunctions/insert-malfunction.php
@@ -1,67 +1,63 @@
-<?php if ($_SERVER['REQUEST_METHOD'] == 'POST') { ?>
-  <!-- start edit profile page -->
-  <div class="container" dir="<?php echo @$_SESSION['systemLang'] == 'ar' ? 'rtl' : 'ltr' ?>">
-    <!-- start header -->
-    <header class="header">
-    
-    <?php
-      // // print $_POST request variables
-      // print_r($_POST);
+<?php 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // get piece info from the form
+  $mng_id         = $_POST['admin-id'];
+  $tech_id        = $_POST['technical-id'];
+  $client_id      = $_POST['client-id'];
+  $descreption    = $_POST['descreption'];
 
-      // get piece info from the form
-      $mng_id         = $_POST['admin-id'];
-      $tech_id        = $_POST['technical-id'];
-      $client_id      = $_POST['client-id'];
-      $descreption    = $_POST['descreption'];
-    
+  // validate the form
+  $formErorr = array();   // error array 
 
-      // validate the form
-      $formErorr = array();   // error array 
-
-      // validate manager id
-      if (empty($mng_id)) {
-        $formErorr[] = 'manager id cannot be <strong>empty</strong>';
-      }
-      // validate technical id
-      if (empty($tech_id)) {
-        $formErorr[] = 'technical id cannot be <strong>empty.</strong>';
-      }
-      // validate username
-      if (empty($client_id)) {
-        $formErorr[] = 'client id cannot be <strong>empty.</strong>';
-      }
-
-      // loop on form error array
-      foreach ($formErorr as $error) {
-        echo '<div class="alert alert-danger text-capitalize w-50 mx-auto align-left">' . $error . '</div>';
-      }
-
-      // check if empty form error
-      if (empty($formErorr)) {
-        // array of malfunction information
-        $mal_info = array();
-        // push info into the array
-        array_push($mal_info, $mng_id, $tech_id, $client_id, $descreption, get_date_now(), get_time_now(), $_SESSION['company_id']);
-        if (!isset($mal_obj)) {
-          // create an object of Malfunction class
-          $mal_obj = new Malfunction();
-        }
-        // call insert function
-        $mal_obj->insert_new_malfunction($mal_info);
-
-        // echo success message
-        $msg = '<div class="alert alert-success text-capitalize"><i class="bi bi-check-circle-fill"></i>&nbsp;'. language('MALFUNCTION WAS ADDED SUCCESSFULLY', @$_SESSION['systemLang']) .'</div>';
-
-        // redirect to add new user
-        redirectHome($msg, 'back');
-      } else {
-        // include permission error module
-        include_once $globmod . 'no-data-founded.php';
-      ?>
-    </header>
-  </div>
-<?php
+  // validate manager id
+  if (empty($mng_id)) {
+    $formErorr[] = 'manager id cannot be empty';
   }
+  // validate technical id
+  if (empty($tech_id)) {
+    $formErorr[] = 'technical id cannot be empty';
+  }
+  // validate username
+  if (empty($client_id)) {
+    $formErorr[] = 'client id cannot be empty';
+  }
+
+  // check if empty form error
+  if (empty($formErorr)) {
+    // array of malfunction information
+    $mal_info = array();
+    // push info into the array
+    array_push($mal_info, $mng_id, $tech_id, $client_id, $descreption, get_date_now(), get_time_now(), $_SESSION['company_id']);
+    
+    if (!isset($mal_obj)) {
+      // create an object of Malfunction class
+      $mal_obj = new Malfunction();
+    }
+    // call insert function
+    $is_inserted = $mal_obj->insert_new_malfunction($mal_info);
+    // check if malfunction was inserted or not
+    if ($is_inserted) {
+      // prepare flash session variables
+      $_SESSION['flash_message'] = 'MALFUNCTION WAS ADDED SUCCESSFULLY';
+      $_SESSION['flash_message_icon'] = 'bi-check-circle-fill';
+      $_SESSION['flash_message_class'] = 'success';
+      $_SESSION['flash_message_status'] = true;
+    } else {    
+      // prepare flash session variables
+      $_SESSION['flash_message'] = 'A PROBLEM WAS HAPPENED WHILE DELETING THE MALFUNCTION';
+      $_SESSION['flash_message_icon'] = 'bi-exclamation-triangle-fill';
+      $_SESSION['flash_message_class'] = 'danger';
+      $_SESSION['flash_message_status'] = false;
+    }
+  } else {
+    // prepare flash session variables
+    $_SESSION['flash_message'] = 'NO DATA FOUNDED';
+    $_SESSION['flash_message_icon'] = 'bi-exclamation-triangle-fill';
+    $_SESSION['flash_message_class'] = 'danger';
+    $_SESSION['flash_message_status'] = false;
+  }
+  // redirect to the previous page
+  redirectHome(null, 'back', 0);
 } else {
   // include permission error module
   include_once $globmod . 'permission-error.php';
