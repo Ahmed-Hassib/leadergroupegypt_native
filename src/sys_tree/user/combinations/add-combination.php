5ecaace005fcde915603ff68c4f0cc0063ee2cfc
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-06-29 01:38:29 +0300

    change condition on buttons depending on employees permissions

--- src/sys_tree/user/combinations/add-combination.php
+++ src/sys_tree/user/combinations/add-combination.php
@@ -8,6 +8,22 @@ $emp_counter = $db_obj->count_records("`UserID`", "`users`", "WHERE `isTech` = 1
 ?>
 <!-- start add new user page -->
 <div class="container" dir="<?php echo @$_SESSION['systemLang'] == 'ar' ? 'rtl' : 'ltr' ?>">
+  <?php if ($_SESSION['comb_add'] == 0) {
+    // prepare flash session variables
+    $_SESSION['flash_message'] = 'NO DATA FOUNDED';
+    $_SESSION['flash_message_icon'] = 'bi-exclamation-triangle-fill';
+    $_SESSION['flash_message_class'] = 'danger';
+    $_SESSION['flash_message_status'] = false;
+    // redirect to the previous page
+    redirectHome(null, 'back', 0);
+  } elseif ($emp_counter == 0) { ?>
+  <!-- note for empty employees -->
+  <div class="alert bg-danger text-white text-capitalize fw-bold">
+    <i class="bi bi-exclamation-triangle-fill"></i>
+    <span><?php echo language('NOTE', @$_SESSION['systemLang']) ?>:</span>&nbsp;
+    <span><?php echo language('YOUR COMPANY HAS NO TECHNICAL MEN, YOU CANNOT ADD ANY COMBINATIONS', @$_SESSION['systemLang']) ?></span>
+  </div>
+  <?php } else { ?>
   <!-- start form -->
   <form class="mb-3 custom-form" action="?do=insert-combination-info" method="POST" id="add-new-combination" onchange="form_validation(this)">
     <!-- horzontal stack -->
@@ -17,13 +33,6 @@ $emp_counter = $db_obj->count_records("`UserID`", "`users`", "WHERE `isTech` = 1
         <span><?php echo language('NOTE', @$_SESSION['systemLang']) ?>:</span>&nbsp;
         <span><?php echo language('THIS SIGN * IS REFERE TO REQUIRED FIELDS', @$_SESSION['systemLang']) ?></span>
       </h6>
-      <?php if ($emp_counter == 0) { ?>
-      <!-- note for empty employees -->
-      <h6 class="h6 text-decoration-underline text-capitalize text-danger fw-bold">
-        <span><?php echo language('NOTE', @$_SESSION['systemLang']) ?>:</span>&nbsp;
-        <span><?php echo language('YOUR COMPANY HAS NO TECHNICAL MEN, YOU CANNOT ADD ANY MALFUNCTIONS', @$_SESSION['systemLang']) ?></span>
-      </h6>
-      <?php } ?>
     </div>
     <div class="row row-cols-sm-1 row-cols-md-2 g-3">
       <!-- first column -->
@@ -58,11 +67,10 @@ $emp_counter = $db_obj->count_records("`UserID`", "`users`", "WHERE `isTech` = 1
                     <option value="<?php echo $userRow['UserID'] ?>">
                       <?php echo $userRow['UserName']; ?>
                     </option>
-                  <?php }
-                } else {
-                  echo "<option disabled>". language("NOT AVAILABLE NOW", @$_SESSION['systemLang']) ."</option>";
-                }
-                ?>
+                  <?php } ?>
+                <?php } else { ?>
+                  <option disabled><?php echo language("NOT AVAILABLE NOW", @$_SESSION['systemLang']) ?></option>
+                <?php } ?>
               </select>
             </div>
           </div>
@@ -108,11 +116,12 @@ $emp_counter = $db_obj->count_records("`UserID`", "`users`", "WHERE `isTech` = 1
     <!-- submit -->
     <div class="mt-3 hstack gap-2">
       <div class="<?php echo @$_SESSION['systemLang'] == 'ar' ? 'me-auto' : 'ms-auto' ?>">
-        <button type="button" form="add-new-combination" class="btn btn-primary text-capitalize form-control bg-gradient fs-12 <?php if ($_SESSION['comb_add'] == 0 || $emp_counter == 0) {echo 'disabled';} ?>" id="add-combination" onclick="form_validation(this.form, 'submit')">
+        <button type="button" form="add-new-combination" class="btn btn-primary text-capitalize form-control bg-gradient fs-12" id="add-combination" onclick="form_validation(this.form, 'submit')">
           <?php echo language('ADD THE COMBINATION', @$_SESSION['systemLang']) ?>
         </button>
       </div>
     </div>
   </form>
   <!-- end form -->
+  <?php } ?>
 </div>
