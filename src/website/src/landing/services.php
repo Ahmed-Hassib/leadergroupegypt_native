f523ec75187966ff5d9ed60ab805fcf9f4f23193
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-07-05 07:08:24 +0300

    change logo of sys tree on website services

--- src/website/src/landing/services.php
+++ src/website/src/landing/services.php
@@ -3,7 +3,7 @@
     <h2 class="main-title"><?php echo language('OUR SERVICES') ?></h2>
     <div class="container">
         <div class="box">
-            <img src="<?php echo $assets ?>systree.jpg" alt="">
+            <img src="<?php echo $website_images ?>sys_tree.jpg" alt="">
             <div class="info">
                 <a class="btn btn-primary" href="<?php echo $sys_tree ?>login.php"><?php echo language('LOGIN') ?></a>
                 <a class="btn btn-outline-success" href="<?php echo $website_pages_desc ?>sys_tree/index.php"><?php echo language('DETAILS') ?></a>
