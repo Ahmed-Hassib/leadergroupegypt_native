eb2bd93ddf750cf1e622590c53d39cae86cc6090
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-07-18 06:16:21 +0300

    create a file to display complaints or suggestions data

---
+++ src/sys_tree/user/comp-sugg/show-comp-sugg.php
@@ -0,0 +1,96 @@
+<?php
+// get type
+$type = isset($_GET['type']) && $_GET['type'] >= 0 ? $_GET['type'] : -1;
+// get period
+$period = isset($_GET['period']) && !empty($_GET['period']) ? $_GET['period'] : -1;
+
+// array of errors
+$errors = array();
+
+// check type
+if ($type < 0) {
+  $errors[] = 'type must be selected';
+}
+
+// check period
+if ($period == -1 || empty($period)) {
+  $errors[] = 'period must be selected';
+}
+
+// check if array of errors is empty
+if (empty($errors)) {
+  // create an object of CompSUgg class
+  if (!isset($comp_sugg_obj)) {
+    $comp_sugg_obj = new CompSugg();
+  }
+  // switch case for period
+  switch ($period) {
+    case 'all':
+      $condition = null;
+      break;
+
+    case 'today':
+      $condition = "`added_date` = '".get_date_now()."'";
+      break;
+
+    case 'month':
+      $start_date = Date('Y-m-1'); 
+      $end_date   = Date('Y-m-31');
+      $condition = "`added_date` BETWEEN '$start_date' AND '$end_date'";
+      break;
+    
+    default:
+      $condition = null;
+      break;
+  }
+
+  // get data
+  $data = $comp_sugg_obj->get_all_data($type, $_SESSION['UserID'], $_SESSION['company_id'], $condition);
+  // check data count
+  if ($data != null && count($data) > 0) {
+?>
+<div class="container" dir="<?php echo @$_SESSION['systemLang'] == 'ar' ? 'rtl' : 'ltr' ?>">
+  <!-- start table container -->
+  <div class="table-responsive-sm">
+    <!-- strst malfunctions table -->
+    <table class="table table-striped table-bordered display compact table-style w-100" id="malfunctions">
+      <thead class="primary text-capitalize">
+        <tr>
+          <th class="text-center" style="width: 20px">#</th>
+          <th class="text-center"><?php echo language('THE TYPE', @$_SESSION['systemLang']) ?></th>
+          <th class="text-center"><?php echo language('THE COMMENT', @$_SESSION['systemLang']) ?></th>
+          <th class="text-center"><?php echo language('ADDED DATE', @$_SESSION['systemLang']) ?></th>
+          <th class="text-center"><?php echo language('ADDED TIME', @$_SESSION['systemLang']) ?></th>
+          <th class="text-center"><?php echo language('CONTROL', @$_SESSION['systemLang']) ?></th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($data as $index => $row) { ?>
+          <tr>
+            <td><?php echo ++$index; ?></td>
+            <td><?php echo $row['type'] == 0 ? language('COMPLAINT', @$_SESSION['systemLang']) : language('SUGGESTION', @$_SESSION['systemLang']) ?></td>
+            <td><?php echo $row['message'] ?></td>
+            <td><?php echo $row['added_date'] ?></td>
+            <td><?php echo date_format(date_create($row['added_time']), 'h:i a') ?></td>
+            <td><?php echo language('CONTROL', @$_SESSION['systemLang']) ?></td>
+          </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+<?php 
+  } else {
+    // include permission error module
+    include_once $globmod . 'no-data-founded.php';
+  }
+} else {
+  foreach ($errors as $key => $error) {
+    $_SESSION['flash_message'][$key] = strtoupper($error);
+    $_SESSION['flash_message_icon'][$key] = 'bi-exclamation-triangle-fill';
+    $_SESSION['flash_message_class'][$key] = 'danger';
+    $_SESSION['flash_message_status'][$key] = false;
+  }
+  // redirect to the previous page
+  redirectHome(null, 'back', 0);
+} ?>
