83cadb8d0e4da9c5e848a71086317d9eeacc820c
Ahmed-Hassib <ahmedhassib2014@gmail.com>
2023-09-26 13:51:20 +0300

    delete unsed classes and add a new class to control website sections

--- classes/website/Section.class.php
+++ classes/website/Section.class.php
@@ -7,6 +7,7 @@ class Section extends Database
 {
   // properties
   public $con;
+  protected $TABLE_NAME = 'sections';
 
   // constructor
   public function __construct()
@@ -16,18 +17,67 @@ class Section extends Database
     $this->con = $db_obj->con;
   }
 
-  // function for login
-  public function user_login()
+  // function for get all sections
+  public function get_all_sections()
   {
-    // select user with specific username and password 
-    $select_user =
-      "SELECT *FROM `users` WHERE `users`.`username` = ? AND `users`.`password` = ? LIMIT 1";
-    // check if user exist in database
-    $stmt = $this->con->prepare($select_user);
+    // select all sections
+    $select_sections = "SELECT *FROM `$this->TABLE_NAME`";
+    // check if sections exist in database
+    $stmt = $this->con->prepare($select_sections);
     $stmt->execute();
-    $user_info = $stmt->fetch();
+    $sections_info = $stmt->fetchAll();
     $count = $stmt->rowCount();
     // check the count
-    return $count > 0 ?  $user_info : null;
+    return $count > 0 ? $sections_info : null;
+  }
+
+  // function to get activated section
+  public function get_activated_sections()
+  {
+    // select all texts
+    $select_texts = "SELECT *FROM `$this->TABLE_NAME` WHERE `is_active` = 1;";
+    $stmt = $this->con->prepare($select_texts);
+    $stmt->execute();
+    $texts_info = $stmt->fetchAll();
+    $count = $stmt->rowCount();
+    // check the count
+    return $count > 0 ? $texts_info : null;
+  }
+
+  // function to deactivate section
+  public function get_deactivated_section()
+  {
+    // select all texts
+    $select_texts = "SELECT *FROM `$this->TABLE_NAME` WHERE `is_active` = 0;";
+    $stmt = $this->con->prepare($select_texts);
+    $stmt->execute();
+    $texts_info = $stmt->fetchAll();
+    $count = $stmt->rowCount();
+    // check the count
+    return $count > 0 ? $texts_info : null;
+  }
+
+  // function to activate section
+  public function activate_section($id)
+  {
+    // update activation query
+    $update_query = "UPDATE `$this->TABLE_NAME` SET `is_active` = 1 WHERE `id` = ?;";
+    $stmt = $this->con->prepare($update_query);
+    $stmt->execute(array($id));
+    $count = $stmt->rowCount();
+    // check the count
+    return $count > 0 ? true : null;
+  }
+
+  // function to deactivate section
+  public function deactivate_section($id)
+  {
+    // update activation query
+    $update_query = "UPDATE `$this->TABLE_NAME` SET `is_active` = 0 WHERE `id` = ?;";
+    $stmt = $this->con->prepare($update_query);
+    $stmt->execute(array($id));
+    $count = $stmt->rowCount();
+    // check the count
+    return $count > 0 ? true : null;
   }
 }
